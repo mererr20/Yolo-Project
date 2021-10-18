61308ee10705509e85540c5c2317f65c4cbec1b7
merelyn-rodriguez <merelyn.rodriguez@omni.cr>
2021-10-17 18:18:51 -0600

    before pull

--- detect.py
+++ detect.py
@@ -79,16 +79,21 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
     pt, onnx, tflite, pb, saved_model = (suffix == x for x in suffixes)  # backend booleans
     stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
     if pt:
+        print("pt 1")
         model = torch.jit.load(w) if 'torchscript' in w else attempt_load(weights, map_location=device)
         stride = int(model.stride.max())  # model stride
         names = model.module.names if hasattr(model, 'module') else model.names  # get class names
-        if half:
+        '''if half:
+            print("pt 2")
             model.half()  # to FP16
         if classify:  # second-stage classifier
+            print("pt 3")
             modelc = load_classifier(name='resnet50', n=3)  # initialize
-            modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()
+            modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()'''
     elif onnx:
+        print("onnx 1")
         if dnn:
+            print("onnx 2")
             # check_requirements(('opencv-python>=4.5.4',))
             net = cv2.dnn.readNetFromONNX(w)
         else:
@@ -96,6 +101,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
             import onnxruntime
             session = onnxruntime.InferenceSession(w, None)
     else:  # TensorFlow models
+        print("=====")
         check_requirements(('tensorflow>=2.4.1',))
         import tensorflow as tf
         if pb:  # https://www.tensorflow.org/guide/migrate#a_graphpb_or_graphpbtxt
@@ -117,15 +123,10 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
             int8 = input_details[0]['dtype'] == np.uint8  # is TFLite quantized uint8 model
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
-    # Dataloader
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
-        bs = len(dataset)  # batch_size
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
-        bs = 1  # batch_size
+
+
+    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
+    bs = 1  # batch_size
     vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
@@ -304,4 +305,6 @@ def main(opt):
 
 if __name__ == "__main__":
     opt = parse_opt()
+    print("opt:")
+    print(opt)
     main(opt)
